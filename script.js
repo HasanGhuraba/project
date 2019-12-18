c1e17a86e77139070ca7353e45e8b0a7696f2423
Hasan <hasan.ere.26@mail.ru>
2019-12-18 11:22:44 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict'; 
+
+var number = 5;
+var string = "hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(); 
+
+let persone = {
+
+    name: "john",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[0]);
+
+// alert("hello World"); 
+
+// let answer = confirm("are you here?");
+
+// confirm.long(answer);
+
+
+let answer = prompt("Есть ли вам 18?", "да");
+
+confirm.long(answer);
